7ac7fe2799a984ea7f77110b9efc5a4b8d716526
Elahe <83521288+elakhatibi@users.noreply.github.com>
2024-03-28 17:45:01 -0700

    Add files via upload

---
+++ movieRec_RAG_MongoDB_project.py
@@ -0,0 +1,43 @@
+import pymongo
+import requests
+from typing import List
+
+client = pymongo.MongoClient("XX")
+db = client.sample_mflix
+collection = db.movies
+
+hf_token = "XX"
+embedding_url = "XX"
+
+def generate_embedding(text: str) -> List[float]:
+
+  response = requests.post(
+    embedding_url,
+    headers={"Authorization": f"Bearer {hf_token}"},
+    json={"inputs": text})
+
+  if response.status_code != 200:
+    raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+
+  return response.json()
+
+# # print(generate_embedding("freecodecamp is awasome!"))
+#generate once
+# for doc in collection.find({'plot':{"$exists": True}}).limit(50):
+#   doc['plot_embedding_hf'] = generate_embedding(doc['plot'])
+#   collection.replace_one({'_id': doc['_id']}, doc)
+
+query = "imaginary characters from outer space at war"
+
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+]);
+
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
